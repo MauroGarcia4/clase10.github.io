595df4acda9f50ff1c63d5d6eaddd54d6c503bcf
MauroGarcia4 <130710305+MauroGarcia4@users.noreply.github.com>
2023-05-19 18:55:34 -0300

    Add files via upload

---
+++ js/javascript.js
@@ -0,0 +1,15 @@
+let nombre = "Mauro"
+let apellido = "Garcia"
+
+console.log(nombre+" "+apellido)
+
+let num1 = 18;
+let num2 = 10;
+
+let suma = num1 + num2;
+
+console.log(suma)
+
+let sumaVariable_Num = (suma+12)
+
+console.log(sumaVariable_Num)
